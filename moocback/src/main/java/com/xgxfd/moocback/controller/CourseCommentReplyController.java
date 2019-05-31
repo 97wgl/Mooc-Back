9a51e911c7282ac4e9415b6a51c91ad6eecf2366
xainzhiyuxue <2310765090@qq.com>
2019-05-31 15:50:29 +0800

    根据type判断id类型，根据name连接获取用户Id  修复教师回复留言显示用户回复的bug

--- moocback/src/main/java/com/xgxfd/moocback/controller/CourseCommentReplyController.java
+++ moocback/src/main/java/com/xgxfd/moocback/controller/CourseCommentReplyController.java
@@ -1,10 +1,16 @@
 package com.xgxfd.moocback.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xgxfd.moocback.entity.CourseCommentReply;
+import com.xgxfd.moocback.entity.Teacher;
+import com.xgxfd.moocback.entity.User;
 import com.xgxfd.moocback.service.CourseCommentReplyService;
 import com.xgxfd.moocback.service.CourseCommentService;
+import com.xgxfd.moocback.service.TeacherService;
+import com.xgxfd.moocback.service.UserService;
 import com.xgxfd.moocback.vo.MessageVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +29,7 @@ import java.time.LocalDateTime;
  * @author Xxz Wgl
  * @since 2019-05-27
  */
+@Slf4j
 @Controller
 @RequestMapping("/course-comment-reply")
 public class CourseCommentReplyController {
@@ -30,13 +37,27 @@ public class CourseCommentReplyController {
     @Autowired
     CourseCommentReplyService courseCommentReplyService;
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    TeacherService teacherService;
+
     @PostMapping("")
     @ResponseBody
     public String postCourseCommentReply(@RequestParam("commentId") Integer commentId ,
                                          @RequestParam("replyUId") Integer replyUId,
                                          @RequestParam("replyToUId") Integer replyToUId,
-                                         @RequestParam("content") String content){
+                                         @RequestParam("content") String content,
+                                         @RequestParam("type") String type){
 
+        log.info(type);
+        if(type.trim().equals("teacher")) { //查找教师对应的用户id
+            String teacherName = teacherService.getById(replyUId).getName();
+            log.info("1.教师Id：" + replyUId +"教师名：" + teacherName);
+            replyUId = userService.getOne(new QueryWrapper<User>().eq("name",teacherName)).getUId();
+        }
+        log.info("2.真正存储的用户Id：" + replyUId);
         CourseCommentReply courseCommentReply = new CourseCommentReply();
         courseCommentReply.setCommentId(commentId);
         courseCommentReply.setReplyUId(replyUId);
